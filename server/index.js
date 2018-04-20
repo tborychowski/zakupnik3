eabd822ad34fb5cc62fb1d2b815d9acddd61a8b2
Tomasz Borychowski <tborychowski@gmail.com>
2018-04-20 14:44:49 +0100

    mongo

--- server/index.js
+++ server/index.js
@@ -13,9 +13,9 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 
-// function sendView (res, view) {
-// 	res.sendFile(view, { root: path.join(__dirname, '..', 'client') });
-// }
+function sendView (res, view) {
+	res.sendFile(view, { root: path.join(__dirname, '..', 'public') });
+}
 
 function isAuthenticated (req, res, next) {
 	if (req.user) return next();
@@ -27,20 +27,20 @@ function isApiAuthenticated (req, res, next) {
 	res.status(401).json({ status: '401' });
 }
 
+app.get('/login', (req, res) => sendView(res, 'login.html'));
+app.get('/logout', (req, res) => { req.logout(); res.redirect('/'); });
+app.post('/login', passport.authenticate('local', { successRedirect: '/', failureRedirect: '/login' }));
+
 app.use('/api/', isApiAuthenticated, require('./api/'));
 
-// app.get('/', (req, res) => sendView(res, 'index.html'));
-app.use('/', express.static(path.join(__dirname, '..', 'public')));
+app.use('/', isAuthenticated, express.static(path.join(__dirname, '..', 'public')));
 
 // app.get('/', isAuthenticated, (req, res) => sendView(res, 'index.html'));
 // app.use('/assets', isAuthenticated, express.static(path.join(__dirname, '..', 'assets')));
 
 
-app.get('/logout', (req, res) => { req.logout(); res.redirect('/'); });
-// app.get('/login', (req, res) => sendView(res, 'login.html'));
-app.post('/login', passport.authenticate('local', { successRedirect: '/', failureRedirect: '/login' }));
 
-
+// old - expressjs way
 // app.listen(3000, () => console.log('Server started.'));
 
 
@@ -48,10 +48,8 @@ app.post('/login', passport.authenticate('local', { successRedirect: '/', failur
 //   -newkey rsa:2048 -nodes -sha256 \
 //   -subj '/CN=localhost' -extensions EXT -config <( \
 //    printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
-
 const key = fs.readFileSync('./server/localhost.key');
 const cert = fs.readFileSync('./server/localhost.crt');
-
 
 spdy
 	.createServer({key, cert}, app)
