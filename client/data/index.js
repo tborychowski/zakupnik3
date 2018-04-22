8c599225a92ddc6081b4f4bfe854a419fd631c38
Tomasz Borychowski <tborychowski@gmail.com>
2018-04-22 13:33:52 +0100

    fixn

--- client/data/index.js
+++ client/data/index.js
@@ -1,34 +1,30 @@
-function get (url) {
-	return fetch(url, { credentials: 'include' }).then(res => res.json());
-}
-
-function post (url, params) {
-	const body = JSON.stringify(params);
+function req (url, method = 'GET', params) {
 	const opts = {
-		method: 'POST',
+		method: method,
 		headers: { 'Content-type': 'application/json' },
 		credentials: 'include',
-		body,
 	};
-	return fetch(url, opts).then(res => res.json());
+	if (params) opts.body = JSON.stringify(params);
+	return fetch(`api/${url}`, opts).then(res => res.json());
 }
 
+const get = url => req(url);
+const post = (url, params) => req(url, 'POST', params);
+const put = (url, params) => req(url, 'PUT', params);
+// const del = (url, params) => req(url, 'DELETE', params);
 
 
 const Categories = {
-	get: () => get('api/categories'),
+	get: () => get('categories'),
 };
 
 const Expenses = {
-	get (date = '') {
-		return get(`api/entries?date=${date}`);
-	},
-	save (data) {
-		if (data.length === 1 && data[0].id) return put('api/entries', data);
-		return post('api/entries', data);
+	get: (date = '') => get(`entries?date=${date}`),
+	save: data => {
+		if (data.length === 1 && data[0].id) return put('entries', data);
+		return post('entries', data);
 	}
 };
-
 
 
 export {
