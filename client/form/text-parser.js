b8e32e6a40264b68e75a19276c43a45d8a110182
Tomasz Borychowski <tborychowski@gmail.com>
2018-05-12 22:16:08 +0100

    make "repeat" work (#17)

--- client/form/text-parser.js
+++ client/form/text-parser.js
@@ -1,3 +1,50 @@
+// yyyy-mm
+const getStrDate = (y, m) => `${y}-${('0' + m).substr(-2)}`;
+
+const deepCopyArray = arr => arr.map(o => Object.assign({}, o));
+
+function parseDateStr (dateStr) {
+	let [year, month] = dateStr.split('-');
+	year = parseInt(year, 10);
+	month = parseInt(month, 10);
+	const str = getStrDate(year, month);
+	return { year, month, str };
+}
+
+function addMonthsToDate (date, monthDiff) {
+	const dateObj = parseDateStr(date);
+	let year = dateObj.year;
+	let month = dateObj.month + monthDiff;
+	if (month >= 13) {
+		year = year + Math.floor(month / 12);
+		month = (month % 12) + 1;
+	}
+	return getStrDate(year, month);
+}
+
+
+function cloneEntries (entries, monthDiff) {
+	return deepCopyArray(entries).map(item => {
+		item.date = addMonthsToDate(item.date, monthDiff);
+		return item;
+	});
+}
+
+
+function repeatEntries (entries, repeater) {
+	if (!repeater || repeater === 1) return entries;
+
+	const originalEntries = deepCopyArray(entries);
+	for (let i = 1; i < repeater; i++) {
+		const newRows = cloneEntries(originalEntries, i);
+		entries = entries.concat(newRows);
+	}
+	return entries;
+}
+
+
+
+
 function parseAmount (amount) {
 	/* eslint no-eval: 0 */
 	amount = ('' + amount).replace(/\s/g, '');
@@ -19,7 +66,7 @@ function findGroup(s, groups) {
 			return [g, s];
 		}
 	}
-	const words = s.split(' ');
+	const words = s.split(' ').filter(w => w.length);
 	for (let w of words) {
 		for (let g of groups) {
 			if (g.keywords.indexOf(w) > -1) {
@@ -28,7 +75,7 @@ function findGroup(s, groups) {
 			}
 		}
 	}
-	return [null, s];
+	return [{}, s];
 }
 
 
@@ -59,9 +106,17 @@ function findAmount(s) {
 
 
 function parse (val, {date, _groups}) {
+	let repeat = 1;
 	const rows = val
 		.split('\n')
 		.map(row => {
+			if (row.toLowerCase().indexOf('repeat') > -1) {
+				const m = row.match(/\d{1,2}/);
+				if (m && m.length) {
+					repeat = parseInt(m[0], 10);
+					return {};
+				}
+			}
 			let group, amount, description;
 			[group, row] = findGroup(row, _groups);
 			[amount, row] = findAmount(row);
@@ -77,7 +132,8 @@ function parse (val, {date, _groups}) {
 	let sum = 0;
 	for (let i = 1; i < rows.length; i++) sum += rows[i].amount;
 	rows[0].amount -= sum;
-	return rows;
+
+	return repeatEntries(rows, repeat);
 }
 
 
