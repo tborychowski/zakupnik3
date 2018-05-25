412b63f1116aa2d054828f959cc05af27435384d
Tomasz Borychowski <tborychowski@gmail.com>
2018-05-25 13:48:23 +0100

    tweaks

--- server/api/stats.js
+++ server/api/stats.js
@@ -6,7 +6,15 @@ const DB = require('../db/stats');
 function get (req, res) {
 	return DB
 		.get(req.params.year, req.query)
-		.then(items => items.map(item => item.sum))
+		.then(items => {
+			const ar = [];
+			items.forEach(item => {
+				const m = parseInt(item.month, 10) - 1;
+				ar[m] = item.sum;
+			});
+			for (let i = 0, l = ar.length; i < l; i++) ar[i] = ar[i] || 0;
+			return ar;
+		})
 		.then(items => res.status(200).json(items))
 		.catch(e => res.status(500).json(e));
 }
