5cdff13b3f52656d952440f0092b39e56e51483a
Tomasz Borychowski <tborychowski@gmail.com>
2018-05-25 01:22:37 +0100

    tweaks

--- server/lib/session.js
+++ server/lib/session.js
@@ -1,5 +1,4 @@
 const session = require('cookie-session');
-const util = require('./util');
 const config = require('./util').getConfig();
 
 //openssl rand -base64 32
@@ -8,8 +7,6 @@ const secret = 'fX5uuWQMpb/z2+yYGfBvfC/ovgpNpPf/7KNUznBGwpg=';
 const sessionCfg = {
 	name: 'zakupnik',
 	secret: config.secret || secret,
-	// secure: util.isProd,
-	sameSite: true,
 	maxAge: 3600000 * 24 * 30 /* 30 days */
 };
 
