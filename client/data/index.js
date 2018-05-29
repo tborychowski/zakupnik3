dceff74ffe5494fb60d5e0c305c9b393b6ed248b
Tomasz Borychowski <tborychowski@gmail.com>
2018-05-29 12:07:56 +0100

    lines

--- client/data/index.js
+++ client/data/index.js
@@ -24,38 +24,35 @@ const save = (url, data) => {
 
 const Categories = {
 	base: 'categories',
-	get (date) { return get(`${this.base}?date=${trimDate(date)}`); },
-	save (data) { return save(this.base, data); },
-	del (id) { return del(`${this.base}/${id}`); }
+	get: date => get(`${this.base}?date=${trimDate(date)}`),
+	save: data => save(this.base, data),
+	del: id => del(`${this.base}/${id}`),
 };
 
 const Groups = {
 	base: 'groups',
-	get () { return get(this.base); },
-	save (data) { return save(this.base, data); },
-	del (id) { return del(`${this.base}/${id}`); }
+	get: () => get(this.base),
+	save: data => save(this.base, data),
+	del: id => del(`${this.base}/${id}`),
 };
 
 const Expenses = {
 	base: 'entries',
-	get (date) { return get(`${this.base}?date=${trimDate(date)}`); },
-	save (data) { return save(this.base, data); },
-	del (id) { return del(`${this.base}/${id}`); }
+	get: date => get(`${this.base}?date=${trimDate(date)}`),
+	save: data => save(this.base, data),
+	del: id => del(`${this.base}/${id}`),
 };
 
 const Stats = {
 	base: 'stats',
-	get (date) {
+	get: date => {
 		const year = date.year || new Date().getFullYear();
 		let url = `${this.base}/${year}`;
-
 		if (date.group) url += `?group=${date.group}`;
 		else if (date.category) url += `?category=${date.category}`;
-
 		return get(url);
 	},
 };
-
 
 export default {
 	Categories,
--- client/index.js
+++ client/index.js
@@ -2,14 +2,11 @@ import App from './app';
 
 new App({ target: document.querySelector('#app') });
 
-
-
-
 function registerSW () {
-	navigator.serviceWorker.register('worker.js')
+	navigator.serviceWorker
+		.register('worker.js')
 		// .then(registration => console.log('SW scope: ', registration.scope))
 		.catch(err => console.log('Worker failed: ', err));
 }
 
 if ('serviceWorker' in navigator) window.addEventListener('load', registerSW);
-
