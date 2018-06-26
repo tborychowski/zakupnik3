48e78810adea08288bf31d05c527b5892a254815
Tomasz Borychowski <tborychowski@gmail.com>
2018-06-27 00:54:56 +0100

    no workers

--- client/index.js
+++ client/index.js
@@ -2,11 +2,11 @@ import App from './app';
 
 new App({ target: document.querySelector('#app') });
 
-function registerSW () {
-	navigator.serviceWorker
-		.register('worker.js')
-		// .then(registration => console.log('SW scope: ', registration.scope))
-		.catch();
-}
+// function registerSW () {
+// 	navigator.serviceWorker
+// 		.register('worker.js')
+// 		// .then(registration => console.log('SW scope: ', registration.scope))
+// 		.catch();
+// }
 
-if ('serviceWorker' in navigator) window.addEventListener('load', registerSW);
+// if ('serviceWorker' in navigator) window.addEventListener('load', registerSW);
