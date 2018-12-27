8af835a2708cee09e818dd2f1998dbc8f9e23421
Tomasz Borychowski <tborychowski@gmail.com>
2018-12-27 11:45:30 +0000

    some store refactored

--- client/data/event.js
+++ client/data/event.js
@@ -5,5 +5,4 @@ export default {
 	off,
 	fire,
 
-
 };
--- client/data/store.js
+++ client/data/store.js
@@ -1,6 +1,8 @@
 import { Store } from 'svelte/store.js';
 
 const store = new Store({
+	date: new Date().toISOString().substr(0, 7),
+	selectedCategory: null,
 });
 
 
