c0ad26be9067a7c9219115105da8914e79a508e9
Tomasz Borychowski <tborychowski@gmail.com>
2018-12-27 21:15:40 +0000

    date picker & chart - done

--- client/data/store.js
+++ client/data/store.js
@@ -1,13 +1,20 @@
 import { Store } from 'svelte/store.js';
 
 const store = new Store({
-	date: new Date().toISOString().substr(0, 7),
+	dateStr: new Date().toISOString().substr(0, 10),
 	category: null,
 	group: null,
 	categories: [],
 	groups: [],
+	input: { group: null, text: null, amount: 0 }
 });
 
+
+store.compute('date', ['dateStr'], dateStr  => {
+	let [y, m, d] = dateStr.split('-');
+	m -= 1;
+	return {y, m, d};
+});
 
 
 export default store;
