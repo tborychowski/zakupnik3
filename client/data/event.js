d18e337c6738235255e0f6521a977c3af8dbef30
Tomasz Borychowski <tborychowski@gmail.com>
2018-12-27 11:26:18 +0000

    refactor data

---
+++ client/data/event.js
@@ -0,0 +1,9 @@
+import {on, off, fire} from './pubsub';
+
+export default {
+	on,
+	off,
+	fire,
+
+
+};
---
+++ client/data/store.js
@@ -0,0 +1,8 @@
+import { Store } from 'svelte/store.js';
+
+const store = new Store({
+});
+
+
+
+export default store;
