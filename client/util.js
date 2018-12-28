c7698c8dd4b597346cf46393af7f121c25e60538
Tomasz Borychowski <tborychowski@gmail.com>
2018-12-28 15:10:07 +0000

    relative dates, sort by date

--- client/util.js
+++ client/util.js
@@ -4,7 +4,21 @@ function formatNumber (num) {
 }
 
 
+function timeAgo (dateStr) {
+	if (!dateStr) return '';
+	const date = new Date(dateStr);
+	const now = new Date(new Date().toISOString().substr(0, 10));
+	const diff = (now - date) / 1000;
+	if (diff < 60) return 'just now';
+	if (diff < 3600) return `${Math.floor(diff / 60)} minutes ago`;
+	if (diff < 86400) return `${Math.floor(diff / 60)} hours ago`;
+	if (diff < 172800) return 'yesterday';
+	if (diff < 604800) return `${Math.floor(diff / 86400)} days ago`;
+	return dateStr;
+}
+
 
 export {
 	formatNumber,
+	timeAgo,
 };
