830afa2e2ea0bc66a10ba5b28f6b8f837a0ab1d6
Tomasz Borychowski <tborychowski@gmail.com>
2019-01-03 11:28:34 +0000

    percent-bar

--- client/data/store.js
+++ client/data/store.js
@@ -11,6 +11,17 @@ const store = new Store({
 });
 
 
+store.compute('categoryTree', ['categories'], categories => {
+	return categories
+		.filter(c => !c.parent_id)
+		.map(c => {
+			c.items = categories.filter(sub => sub.parent_id ===  c.id);
+			return c;
+		})
+		.sort((a, b) => b.income - a.income);
+});
+
+
 store.compute('categoryNameMap', ['categories'], categories => {
 	const map = {};
 	categories.forEach(c => { map[c.id] = c.name;});
